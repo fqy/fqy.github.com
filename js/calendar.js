478ba5565739627dc6dde8350f9e2ac3192af528
fqy <fengqy0000@gmail.com>
2013-04-05 22:44:54 +0800

    revise

--- js/calendar.js
+++ js/calendar.js
@@ -22,7 +22,7 @@ function calendar()
 	//write第一周
 	for(c = first_day; c < 7; c ++)
 	{
-		cal.rows[r].cells[c] = d;
+		cal.rows[r].cells[c].innerHTML = d;
 		d ++;
 	}
 	r++;
@@ -31,7 +31,7 @@ function calendar()
 	{
 		for(c = 0; c < 7; c ++)
 		{
-			cal.rows[r].cells[c] = d;
+			cal.rows[r].cells[c].innerHTML = d;
 			d ++;
 		}
 		r++;
@@ -39,7 +39,7 @@ function calendar()
 	//write最后一周
 	for(c = 0; c < last_day + 1; c ++)
 	{
-		cal.rows[r].cells[c] = d;
+		cal.rows[r].cells[c].innerHTML = d;
 		d ++;
 	}
 }
