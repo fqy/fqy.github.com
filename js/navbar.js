7b3fecd445f69a5223923501e8b35bea1100b5f6
fqy <fengqy0000@gmail.com>
2013-05-17 21:00:28 +0800

    revise

---
+++ js/navbar.js
@@ -0,0 +1,31 @@
+/* 导航栏动画 */
+
+/* 主页块 */
+function home_mouseover()
+{
+	document.getElementById('nav_home').src = "/image/home_word.png";
+}
+function home_mouseout()
+{
+	document.getElementById('nav_home').src = "/image/home.png";
+}
+
+/* 日志块 */
+function journal_mouseover()
+{
+	document.getElementById('nav_journal').src = "/image/journal_word.png";
+}
+function journal_mouseout()
+{
+	document.getElementById('nav_journal').src = "/image/journal.png";
+}
+
+/* fqy块 */
+function fqy_mouseover()
+{
+	document.getElementById('nav_fqy').src = "/image/fqy_word.png";
+}
+function fqy_mouseout()
+{
+	document.getElementById('nav_fqy').src = "/image/fqy.png";
+}
